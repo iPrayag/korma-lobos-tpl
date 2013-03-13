fbee07f3ea95e72db595ed6e92b34cb930f8b218
Prayag <prayag.upd@gmail.com>
2013-03-13 11:17:41 +0545

    REFACTOR secret-sample.clj to secret.clj as config.clj is using ns secret

--- src/korma_lobos_tpl/secret-sample.clj
+++
@@ -1,8 +0,0 @@
-(ns korma-lobos-tpl.secret-sample)
-
-(def ^:dynamic *db-name* "")
-(def ^:dynamic *db-user* "")
-(def ^:dynamic *db-password* "")
-(def ^:dynamic *db-host* "127.0.0.1")
-(def ^:dynamic *db-port* 3306)
-
