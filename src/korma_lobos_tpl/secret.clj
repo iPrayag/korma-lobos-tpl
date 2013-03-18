33b7250e977cfc0878f8e5da317ae4b29c5a9aec
Prayag <prayag.upd@gmail.com>
2013-03-18 09:56:05 +0545

    ADD secret.clj for db conf

---
+++ src/korma_lobos_tpl/secret.clj
@@ -0,0 +1,8 @@
+(ns korma-lobos-tpl.secret)
+
+(def ^:dynamic *db-name* "tpl")
+(def ^:dynamic *db-user* "root")
+(def ^:dynamic *db-password* "mysql55")
+(def ^:dynamic *db-host* "127.0.0.1")
+(def ^:dynamic *db-port* 3306)
+
